3b8e9e7123ab10e1d78dd96ea2a6b75dc26140fc
naub7473 <Albin*230209>
2016-10-05 11:29:53 +0200

    Fixing crash on creation of "Webcom" object
     dur to wrong app inheritance

--- app/src/main/java/com/orange/datasync/chat/ChatApp.java
+++ app/src/main/java/com/orange/datasync/chat/ChatApp.java
@@ -7,7 +7,7 @@ import com.orange.webcom.sdk.internal.util.Log;
 /**
  * Useless here but serves as an example
  */
-public class ChatApp extends Application {
+public class ChatApp extends com.orange.webcom.sdk.internal.App {
     @Override
     public void onCreate() {
         super.onCreate();
