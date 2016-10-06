fa90c51ed3dabcf8ec13241c3d147cf3f660b1fd
Julien Delage <julien.delage@orange.com>
2016-10-06 11:21:12 +0200

    chore: Update Webcom dependency

--- app/src/main/java/com/orange/datasync/chat/ChatApp.java
+++ app/src/main/java/com/orange/datasync/chat/ChatApp.java
@@ -2,15 +2,23 @@ package com.orange.datasync.chat;
 
 import android.app.Application;
 
+import com.orange.webcom.sdk.Config;
 import com.orange.webcom.sdk.internal.util.Log;
 
 /**
  * Useless here but serves as an example
  */
-public class ChatApp extends com.orange.webcom.sdk.internal.App {
+public class ChatApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Config.onCreate(this);
         Log.i("ChatApp", "Created");
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        Config.onTerminate();
     }
 }
